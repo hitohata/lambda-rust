28adc53c1dce7deeda5f0023f88ae11321cbc7b7
hirohata <hirohatatro@gmail.com>
2024-02-01 18:57:34 -0800

    added ts lambda

--- lib/lambda-rust-stack.ts
+++ lib/lambda-rust-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from "aws-cdk-lib/aws-apigateway"
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
 
 export class LambdaRustStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new NodejsFunction(this, "node-lambda", {
+      entry: "lambda/ts/src/index.ts",
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_18_X,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaRustQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new apigw.LambdaRestApi(this, "api", {
+      handler: fn,
+    })
   }
 }
